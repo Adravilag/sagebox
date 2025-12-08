1f923bf86cc54818e9e78ab0334a3c2e798d094b
Adrián Dávila <adrian.davilaguerra@gmail.com>
2025-12-08 17:03:40 +0100

    fix(modal): fix require is not defined in storybook config

--- packages/modal/.storybook/main.ts
+++ packages/modal/.storybook/main.ts
@@ -1,11 +1,14 @@
-import { dirname, join } from "node:path";
+import { dirname, join } from 'node:path';
+import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/html-vite';
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath("@storybook/addon-docs")],
+  addons: [getAbsolutePath('@storybook/addon-docs')],
   framework: {
-    name: getAbsolutePath("@storybook/html-vite"),
+    name: getAbsolutePath('@storybook/html-vite'),
     options: {},
   },
   staticDirs: ['../dist'],
@@ -17,5 +20,5 @@ const config: StorybookConfig = {
 export default config;
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
