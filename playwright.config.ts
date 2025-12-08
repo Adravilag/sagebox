37da49717f6f91760cc373a4ca65ced1f48c0811
Adrián Dávila <adrian.davilaguerra@gmail.com>
2025-12-08 18:13:17 +0100

    test: exclude website tests from main e2e config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  /* Ignore website tests - they require a separate webserver config */
+  testIgnore: ['**/website/**'],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
